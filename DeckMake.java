58f54f2560d181793f54bfdbc1ce6d37318d7db2
Yuta Matayoshi <yuuta.matayoshi1413@gmail.com>
2024-07-26 11:02:03 +0900

    add DeckMake
    デッキ作成、シャッフルの機能追加

---
+++ DeckMake.java
@@ -0,0 +1,54 @@
+import net.goui.util.MTRandom;
+
+public class DeckMake {
+    private static final int NUM_DECKS = 6;
+    private static final int CARDS_PER_DECK = 52;
+    private static final int TOTAL_CARDS = NUM_DECKS * CARDS_PER_DECK;
+    private static final String[] SUITS = { "Hearts", "Diamonds", "Clubs", "Spades" };
+    private static final String[] RANKS = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+    
+    private String[] deck = new String[TOTAL_CARDS];
+    private double rnd;
+    
+    public DeckMake(double rnd) {
+        this.rnd = rnd;
+        initializeDeck();
+        shuffleDeck();
+    }
+    //デッキの初期化
+    private void initializeDeck() {
+        int index = 0;
+        for (int i = 0; i < NUM_DECKS; i++) {
+            for (String suit : SUITS) {
+                for (String rank : RANKS) {
+                    deck[index++] = rank + " : " + suit;
+                }
+            }
+        }
+    }
+    //デッキのシャッフル
+    private void shuffleDeck() {
+        MTRandom random = new MTRandom((long) (rnd * 1e9)); // rndをシードとして使用
+        for (int i = TOTAL_CARDS - 1; i > 0; i--) {
+            int j = random.nextInt(i + 1);
+            String temp = deck[i];
+            deck[i] = deck[j];
+            deck[j] = temp;
+        }
+    }
+    //デッキの表示
+    public void printDeck() {
+        for (String card : deck) {
+            System.out.println(card);
+        }
+    }
+
+    public static void main(String[] args) {
+        final long seed = Math.round(Math.random() * 1e9);
+        double drnd = 0.0;
+        MTRandom rnd = new MTRandom(seed);
+        drnd = rnd.nextDouble();
+        DeckMake cardDeck = new DeckMake(drnd);
+        cardDeck.printDeck();
+    }
+}
--- swicthtest.java
+++ swicthtest.java
@@ -1,61 +1,66 @@
 public class swicthtest {
 
-    public int dorrow(int bankerall, int playerall) {
-        int i = 0;
-        switch (playerall){
+    public static int dorrow(int bankerall, int playerthird) {
+        int thd = 0;
+        switch (playerthird){
             case 10:
-                i = 5;
+                thd = 5;
                 break;
+            case 0:
             case 1:
-                i = 3;
+                thd = 3;
                 break;
             case 2:
             case 3:
-                i = 4;
+                thd = 4;
                 break;
             case 4:
             case 5:
-                i = 5;
+                thd = 5;
                 break;
             case 6:
             case 7:
-                i = 6;
+                thd = 6;
                 break;
             case 8:
-                i = 2;
+                thd = 2;
                 break;
             case 9:
-                i = 3;
+                thd = 3;
                 break;
         }
+        if (bankerall <= thd){
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 
-        return n;
-
+    public static int dorrow2(int bankerall, int playerthird) {
+        final int H = 1;
+        final int S = 0;
+        final int[][] table = {
+            { H, H, H, H, H, H, H, H, H, H, H },
+            { H, H, H, H, H, H, H, H, H, H, H },
+            { H, H, H, H, H, H, H, H, H, H, H },
+            { H, H, H, H, H, H, H, H, H, S, H },
+            { H, S, S, H, H, H, H, H, H, S, H },
+            { H, S, S, S, S, H, H, H, H, S, S },
+            { S, S, S, S, S, S, S, H, H, S, S },
+            { S, S, S, S, S, S, S, S, S, S, S }
+        };
+        return (table[bankerall][playerthird]);
     }
     public static void main(String[] args) {
-        int bankerall = 0;
-        int playerall = 0;
+        int bankerall = 4;
+        int playerthird = 8;
 
-        switch (bankerall) {
-            case 0:
-                if (playerall === )
-                break;
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            case 5:
-                break;
-            case 6:
-                break;
-            case 7:
-                break;
-            default:
-                break;
+        int result = dorrow2(bankerall, playerthird);
+        if (result == 1){
+            System.out.println("Banker should draw");
+        } else {
+            System.out.println("Banker should not draw");
         }
+        ;
     }
 }
